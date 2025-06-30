c617d4dd4c8a0008377425cefe4f71dd25195801
VikasVaghasiya2806 <VikasVaghasiya2806@gmail.com>
2025-06-30 15:04:07 +0530

    Add files via upload

---
+++ Convert.py
@@ -0,0 +1,18 @@
+# Convert from int to float:
+x = float(1)   # 1 becomes 1.0
+
+# Convert from float to int:
+y = int(2.8)   # 2.8 becomes 2 (decimal part is removed)
+
+# Convert from int to complex:
+z = complex(1) # 1 becomes (1+0j), a complex number
+
+# Print the converted values
+print(x)       # 1.0
+print(y)       # 2
+print(z)       # (1+0j)
+
+# Print the data types of each variable
+print(type(x)) # <class 'float'>
+print(type(y)) # <class 'int'>
+print(type(z)) # <class 'complex'>
